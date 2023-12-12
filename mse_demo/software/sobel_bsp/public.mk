f0bf45696579b82fc5060c8e459546fdb5e702f5
arey <adrien.rey@outlook.com>
2023-12-12 08:56:57 +0100

    16K I/D cache added

--- mse_demo/software/sobel_bsp/public.mk
+++ mse_demo/software/sobel_bsp/public.mk
@@ -157,9 +157,9 @@ SOPC_SYSID_FLAG += --sidp=0x10010d8
 ELF_PATCH_FLAG  += --sidp 0x10010d8
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1702203777
-SOPC_SYSID_FLAG += --timestamp=1702203777
-ELF_PATCH_FLAG  += --timestamp 1702203777
+# setting SOPC_TIMESTAMP is 1702367291
+SOPC_SYSID_FLAG += --timestamp=1702367291
+ELF_PATCH_FLAG  += --timestamp 1702367291
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
